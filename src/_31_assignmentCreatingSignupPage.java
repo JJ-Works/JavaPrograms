d165756dc872073e98680092d823e1ddeed9f08d
JJ <janarjanrb@gmail.com>
2025-04-17 00:14:40 +0545

    Java application UI Finished

--- src/_31_assignmentCreatingSignupPage.java
+++ src/_31_assignmentCreatingSignupPage.java
@@ -9,39 +9,47 @@ import java.awt.event.*;
 public class _31_assignmentCreatingSignupPage {
     public static void main(String[] args) {
 
-
-       JFrame frame = new JFrame();
+        //----------------Frame creation---------------------//
+        JFrame frame = new JFrame();
         frame.setLayout(null);
-       frame.setSize(950,1000);
-       frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(800, 1000);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
-       Border border = BorderFactory.createLineBorder(Color.black,3,true);
+    //--------------------Border Creation--------------------//
+        Border border = BorderFactory.createLineBorder(Color.black, 3, true);
         JPanel cardPanel = new JPanel();
-        cardPanel.setBackground(Color.LIGHT_GRAY);
-        cardPanel.setBounds(100,50,700,700);
+        cardPanel.setBackground(new Color(220,240,240));
+        cardPanel.setBounds(100, 50, 550, 700);
         cardPanel.setBorder(border);
 
+
+    //------------------Panel Creation------------------------//
         JPanel panelNav = new JPanel();
-        panelNav.setSize(700,100);
-        panelNav.setBackground(new Color(200,20,25));
-//        panelNav.setBounds(20,0,700,100);
+        panelNav.setSize(550, 80);
+        panelNav.setBackground(new Color(200, 150, 25));
 
 
+    //------------------Main Components Creation------------------//
 
 
-        JLabel labelTitle = new JLabel("--------------| Sign Up |--------------");
-        labelTitle.setFont(new Font("Helvetica",Font.BOLD,40));
+        JLabel labelTitle = new JLabel("-----| Sign Up |-----");
+        labelTitle.setFont(new Font("Impact", Font.BOLD, 45));
 
+        //---------For name---------------//
+        JLabel labelName = new JLabel();
+        labelName.setText("Name: ");
+        labelName.setBounds(10,100,100,30);
+        labelName.setFont(new Font("Helvetica",Font.BOLD,20));
+        cardPanel.add(labelName);
 
         JTextField textFieldName = new JTextField();
-        Border borderText = BorderFactory.createLineBorder(Color.BLACK,1,true);
-                    textFieldName.setText("Enter");
+        Border borderText = BorderFactory.createLineBorder(Color.BLACK, 1, true);
+        textFieldName.setText("Enter");
 
 
         textFieldName.setColumns(25);
-        textFieldName.setFont(new Font("Helvetica",Font.BOLD,20));
-        textFieldName.setBackground(Color.red);
+        textFieldName.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        textFieldName.setBackground(Color.WHITE);
         textFieldName.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -50,9 +58,9 @@ public class _31_assignmentCreatingSignupPage {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(textFieldName.getText().equals("Enter")){
+                if (textFieldName.getText().equals("Enter")) {
                     textFieldName.setText("");
-                    textFieldName.setBackground(Color.GREEN);
+                    textFieldName.setBackground(Color.lightGray);
                 }
             }
 
@@ -63,23 +71,26 @@ public class _31_assignmentCreatingSignupPage {
         });
 
 
-
-
-
-
         textFieldName.setBorder(borderText);
-        textFieldName.setBounds(10,120,500,30);
+        textFieldName.setBounds(10, 130, 500, 30);
 
 
 
+        //----------For userName--------------//
+        JLabel labelUsername = new JLabel();
+        labelUsername.setBounds(10,170,200,30);
+        labelUsername.setText("Username: ");
+        labelUsername.setFont(new Font("Helvetica",Font.BOLD,20));
+        cardPanel.add(labelUsername);
 
-        JTextField textFieldUsername = new JTextField();
+
+       JTextField textFieldUsername = new JTextField();
         textFieldUsername.setText("Enter");
 
 
         textFieldUsername.setColumns(25);
-        textFieldUsername.setFont(new Font("Helvetica",Font.BOLD,20));
-        textFieldUsername.setBackground(Color.red);
+        textFieldUsername.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        textFieldUsername.setBackground(Color.white);
         textFieldUsername.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -88,9 +99,9 @@ public class _31_assignmentCreatingSignupPage {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(textFieldUsername.getText().equals("Enter")){
+                if (textFieldUsername.getText().equals("Enter")) {
                     textFieldUsername.setText("");
-                    textFieldUsername.setBackground(Color.GREEN);
+                    textFieldUsername.setBackground(Color.LIGHT_GRAY);
                 }
             }
 
@@ -100,13 +111,16 @@ public class _31_assignmentCreatingSignupPage {
             }
         });
 
-
-
-
-
-
         textFieldUsername.setBorder(borderText);
-        textFieldUsername.setBounds(10,170,500,30);
+        textFieldUsername.setBounds(10, 200, 500, 30);
+
+        //---------------------For Email------------------//
+
+        JLabel labelEmail = new JLabel();
+        labelEmail.setText("Email: ");
+        labelEmail.setFont(new Font("Helvetica",Font.BOLD,20));
+        labelEmail.setBounds(10,240,100,30);
+        cardPanel.add(labelEmail);
 
 
         JTextField textFieldEmail = new JTextField();
@@ -114,8 +128,8 @@ public class _31_assignmentCreatingSignupPage {
 
 
         textFieldEmail.setColumns(25);
-        textFieldEmail.setFont(new Font("Helvetica",Font.BOLD,20));
-        textFieldEmail.setBackground(Color.red);
+        textFieldEmail.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        textFieldEmail.setBackground(Color.white);
         textFieldEmail.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -124,9 +138,9 @@ public class _31_assignmentCreatingSignupPage {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(textFieldEmail.getText().equals("Enter")){
+                if (textFieldEmail.getText().equals("Enter")) {
                     textFieldEmail.setText("");
-                    textFieldEmail.setBackground(Color.GREEN);
+                    textFieldEmail.setBackground(Color.LIGHT_GRAY);
                 }
             }
 
@@ -137,61 +151,122 @@ public class _31_assignmentCreatingSignupPage {
         });
 
 
-
-
-
-
         textFieldEmail.setBorder(borderText);
-        textFieldEmail.setBounds(10,200,500,30);
+        textFieldEmail.setBounds(10, 270, 500, 30);
 
+        //------------For Password-----------------//
+        JLabel labelPassword = new JLabel();
+        labelPassword.setText("Password: ");
+        labelPassword.setFont(new Font("Helvetica",Font.BOLD,20));
+        labelPassword.setBounds(10, 310, 150, 30);
+        cardPanel.add(labelPassword);
 
-
-
-
-        JTextField textFieldPassword = new JTextField();
+        JPasswordField textFieldPassword = new JPasswordField();
         textFieldPassword.setText("Enter");
 
 
         textFieldPassword.setColumns(25);
-        textFieldPassword.setFont(new Font("Helvetica",Font.BOLD,20));
-        textFieldPassword.setBackground(Color.red);
-        textFieldPassword.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if(textFieldPassword.getText().equals("Enter")){
-                    textFieldPassword.setText("");
-                    textFieldPassword.setBackground(Color.GREEN);
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        });
-
-
-
-
+        textFieldPassword.setFont(new Font("Helvetica", Font.PLAIN, 15));
+        textFieldPassword.setBackground(Color.white);
+//        textFieldPassword.addKeyListener(new KeyListener() {
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//
+//            }
+//
+////            @Override
+////            public void keyPressed(KeyEvent e) {
+////                if (textFieldPassword.getText().equals("Enter")) {
+////                    textFieldPassword.setText("");
+////                    textFieldPassword.setBackground(Color.GREEN);
+////                }
+////            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {
+//
+//            }
+//        });
 
 
         textFieldPassword.setBorder(borderText);
-        textFieldPassword.setBounds(10,250,500,30);
+        textFieldPassword.setBounds(10, 340, 500, 30);
+
+        //-------------For Gender--------------//
+        JLabel labelGender = new JLabel();
+        labelGender.setText("Gender:");
+        labelGender.setBounds(10,380,100,30);
+        labelGender.setFont(new Font("Helvetica",Font.BOLD,20));
+        cardPanel.add(labelGender);
+
+        JCheckBox checkBox = new JCheckBox();
+        checkBox.setBounds(90,382,20,30);
+        cardPanel.add(checkBox);
+        checkBox.setBackground(new Color(220,240,240));
+
+        JLabel labelMale = new JLabel();
+        labelMale.setText("Male");
+        labelMale.setFont(new Font("Helvetica",Font.BOLD,15));
+        labelMale.setBounds(114,382,40,30);
+        cardPanel.add(labelMale);
+
+        JCheckBox checkBoxFem = new JCheckBox();
+        checkBoxFem.setBounds(150,382,20,30);
+        cardPanel.add(checkBoxFem);
+        checkBoxFem.setBackground(new Color(220,240,240));
+
+        JLabel labelFemale = new JLabel();
+        labelFemale.setText("Female");
+        labelFemale.setFont(new Font("Helvetica",Font.BOLD,15));
+        labelFemale.setBounds(178,382,70,30);
+        cardPanel.add(labelFemale);
+
+        JCheckBox checkBoxOther = new JCheckBox();
+        checkBoxOther.setBounds(237,382,20,30);
+        cardPanel.add(checkBoxOther);
+        checkBoxOther.setBackground(new Color(220,240,240));
+
+        JLabel labelOthers = new JLabel();
+        labelOthers.setText("Others");
+        labelOthers.setFont(new Font("Helvetica",Font.BOLD,15));
+        labelOthers.setBounds(260,382,70,30);
+        cardPanel.add(labelOthers);
 
 
 
-        JButton buttonName3 = new JButton("Submit");
-        buttonName3.setBackground(Color.cyan);
-        buttonName3.setBounds(350,500,100,30);
-        buttonName3.setBorder(new LineBorder(null,1,true));
 
 
 
+//---------------Current Semester------------------//
+        JLabel labelSemester = new JLabel();
+        labelSemester.setText("Semester:");
+        labelSemester.setFont(new Font("Helvetica",Font.BOLD,20));
+        labelSemester.setBounds(10,425,150,30);
+        cardPanel.add(labelSemester);
+
+
+        String[] semesters = {"Semester 1","Semester 2","Semester 3","Semester 4","Semester 5","Semester 6","Semester 7","Semester 8"};
+        JComboBox<String> comboBox = new JComboBox<String>(semesters);
+        comboBox.setFont(new Font("Helvetica",Font.PLAIN,20));
+        comboBox.setBounds(110,428,170,30);
+//
+//        Border bodForSem = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.white,1),
+//        new EmptyBorder(1,1,1,1)
+//    );
+
+        Border bodForSem = BorderFactory.createLineBorder(Color.cyan,01,true);
+        comboBox.setBorder(bodForSem);
+
+        cardPanel.add(comboBox);
+
+
+
+
+        JButton button = new JButton("Submit");
+        button.setFont(new Font("Helvetic",Font.BOLD,30));
+        button.setBackground(Color.cyan);
+        button.setBounds(210, 500, 150, 60);
+        button.setBorder(new LineBorder(Color.gray, 1, true));
 
 
         //------------------------ADDER------------------------//
@@ -203,13 +278,25 @@ public class _31_assignmentCreatingSignupPage {
         cardPanel.add(textFieldUsername);
         cardPanel.add(textFieldPassword);
         cardPanel.add(textFieldEmail);
-        cardPanel.add(buttonName3);
+        cardPanel.add(button);
         panelNav.add(labelTitle);
         frame.add(cardPanel);
-       frame.setVisible(true);
+        frame.setVisible(true);
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -259,15 +346,15 @@ public class _31_assignmentCreatingSignupPage {
 //        // The things that we add i frames is known as a components.
 //        // ImageIcon icon = new ImageIcon("asmt.jpg");
 //        // frame.setIconImage(icon.getImage());
-////
-////        JLabel label = new JLabel("This is a label");
-////        label.setSize(200,300);
-////        label.setHorizontalAlignment(JLabel.CENTER);
-////        label.setVerticalAlignment(JLabel.TOP);
-////        label.setFont(new Font("Helvetica",Font.BOLD,20));
-////
-////        // If you are using no layout method then you must set bounds manually inorder to manage the componentes in the Panel, frame.
-////        frame.add(label);
+/// /
+/// /        JLabel label = new JLabel("This is a label");
+/// /        label.setSize(200,300);
+/// /        label.setHorizontalAlignment(JLabel.CENTER);
+/// /        label.setVerticalAlignment(JLabel.TOP);
+/// /        label.setFont(new Font("Helvetica",Font.BOLD,20));
+/// /
+/// /        // If you are using no layout method then you must set bounds manually inorder to manage the componentes in the Panel, frame.
+/// /        frame.add(label);
 //
 //        // Now lets talk about Panels
 //        // Panels are similar to divs and frame. Frame are the windows but panels are the area we can create to insert components to.
